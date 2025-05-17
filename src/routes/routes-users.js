8e73d759200589628b1ea89cd04aca41f4e0decc
Gabryel-Modesto <gabryel.modesto@aluno.senai.br>
2025-05-17 16:01:05 -0300

    Rota de Update e Delete concluidas

--- src/routes/routes-users.js
+++ src/routes/routes-users.js
@@ -4,6 +4,7 @@ import db from '../db/db.js'
 
 const router = express.Router();
 
+
 router.get('/', (req, res) => {
     res.send("Olaaaaa");
 });
@@ -21,7 +22,7 @@ router.get('/users', (req, res) => {
 });
 
 
-router.post('/users/insertusers', (req, res) => {
+router.post('/users/insertusers/', (req, res) => {
     const {name_user, email_user} = req.body;
 
     const sql = `INSERT INTO users (name_user, email_user) VALUES (?, ?)`;
@@ -35,6 +36,48 @@ router.post('/users/insertusers', (req, res) => {
         res.redirect('/');
     });
 
+});
+
+
+router.put('/users/updateusers/:id_user', (req, res) => {
+    const id_user = parseInt(req.params.id_user);
+    const {name_user, email_user} = req.body;
+
+    const sql = `UPDATE users SET name_user = COALESCE(?, name_user),
+                email_user = COALESCE(?, email_user)
+                WHERE id_user = ?`;
+
+    db.query(sql, [name_user, email_user, id_user], (err, result) => {
+        if(err){
+            console.error(err);
+            return res.status(500).send('Erro ao atulizar o usuário');
+        };
+
+        if(result.affectedRows === 0) {
+            return res.status(400).json({mensagem: 'Usuário não encontrado'});
+        };
+        
+        res.status(200).json({mensagem: 'Usuário atualizado com sucesso!'});
+    });
+});
+
+router.delete('/users/deleteusers/:id_user', (req, res) => {
+    const id_user = parseInt(req.params.id_user);
+
+    const sql = `DELETE FROM users WHERE id_user = ?`;
+    
+    db.query(sql, [id_user], (err, result) => {
+        if(err ) {
+            console.error(err);
+            return res.status(500).json({mensagem: 'Erro ao deletar usuário'});
+        };
+
+        if(result.affectedRows === 0) {
+           return res.status(400).json({mensagem: 'Usuário não encontrado'});
+        };
+
+        res.status(200).json({mensagem: 'Usuário deletado com sucesso!'});
+    });
 });
 
 export default router;
